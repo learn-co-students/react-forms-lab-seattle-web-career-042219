2de31ecdc1bc9353cea8da1e6f5a49c0cef347c6
Sean Watters <sean@watters.io>
2019-07-01 15:41:47 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,46 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {};
+  state = {
+    username: '',
+    password: ''
   }
+
+  changeUsername = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  changePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (!this.state.username || !this.state.password) {
+      return
+    }
+    this.props.handleLogin(this.state)
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.changeUsername} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.changePassword} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  state = {
+    message: ''
+  };
 
-    this.state = {};
+  changeMessage = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.changeMessage} value={this.state.message}/>
+        {this.props.maxChars-this.state.message.length}
       </div>
     );
   }
